18551e6ca5cc66337b663eaa2c5e8972ec17a6bf
ramzy05 <ramsesaguegang@gmail.com>
2022-05-24 07:16:37 +0000

    changement des html en php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Mairie</title>
-  <link rel="stylesheet" href="./main/styles/general.css">
+  <link rel="stylesheet" href="./css/general.css">
 </head>
 <body>
 
