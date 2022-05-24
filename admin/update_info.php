887b22010f873e0d3401f9b714e38b8e9a4c3016
ramzy05 <ramsesaguegang@gmail.com>
2022-05-24 16:13:13 +0000

    add and update page info finished

---
+++ admin/update_info.php
@@ -0,0 +1,45 @@
+<?php require_once("./includes/head.php"); ?>
+
+  <!-- header section -->
+  <?php require_once("./includes/header.php"); ?>
+  <!-- end header section -->
+  
+
+    <!-- sidebar section -->
+    <?php require_once("./includes/db.php"); ?>
+    <!-- end sidebar section -->
+  <main>
+  <?php require_once("./includes/sidebar.php"); ?>
+  <section id="content">
+    <h3 class="ad_title">Modification des Informations</h3><br>
+
+      <div class="content">
+      <form action="">
+        <div class="form_inp">
+          <label for="nom_mairie">Nom de la mairie</label>
+          <input type="text" name="msg_welc">
+        </div>
+        <div class="form_inp">
+          <label for="msg_welc">Message de bienvenue</label>
+          <textarea name="msg_welc" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div class="form_inp">
+          <label for="logo">Votre logo</label>
+          <input type="file" name="logo">
+        </div>
+        <div class="form_inp controls">
+          <button type="submit">Valider</button>
+          <button type="submit">Annuler</button>
+        </div>
+      </form>
+    </div>
+    
+  </section>
+    <!-- end content section -->
+
+    <!-- footer sec -->
+
+</main>
+
+
+<?php require_once("./includes/footer.php") ?>
