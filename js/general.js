8d42f716e00773c7aa6bab52e9195d5125f5bcc3
ramzy05 <ramsesfeukeng@mail.com>
2022-06-08 23:14:36 +0000

    changement de theme effectif

--- js/general.js
+++ js/general.js
@@ -18,26 +18,3 @@ window.addEventListener("scroll", ()=>{
 /* active menu */
 let links = document.querySelectorAll('.links')
 let sections = document.querySelectorAll('section')
-
-/* function activeMenu(){
-  let len = sections.length
-  while(--len && window.scrollY + 96 < sections[len].offsetTop){}
-  links.forEach(link => link.classList.remove("active"))
-  links[len].classList.add("active")
-} */
-
-/* for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function() {
-    let current = document.getElementsByClassName("active");
-
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace("active", "");
-    }
-
-   
-    this.className += " active";
-  });
-}
- */
-
-/* end active menu */
