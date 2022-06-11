2f22a5937421ee991f46ce90508b56f4070e0d43
ramzy05 <ramsesfeukeng@mail.com>
2022-06-11 15:35:01 +0000

    lecture de la description dun lieu touristique

--- tourisme.php
+++ tourisme.php
@@ -3,39 +3,39 @@
 
 <!-- end head -->
 
-  <!-- header section -->
-  <?php require_once("./includes/header.php"); ?>
-  
-  <!-- end header section -->
-  
-  <!-- main  -->
-  <main>
+<!-- header section -->
+<?php require_once("./includes/header.php"); ?>
+
+<!-- end header section -->
+
+<!-- main  -->
+<main>
   <?php require_once("./includes/db.php"); ?>
 
-   
-    <!-- presentation section -->
-    <section id="tourisme">
-      <div class="tourisme container">
+
+  <!-- presentation section -->
+  <section id="tourisme">
+    <div class="tourisme container">
       <div class="tourisme_header">
         <h1 class="title">Lieux Touristiques</h1>
       </div>
-        <div class="all_lieux">
-        <?php 
-              $q = " SELECT * FROM lieu_touristique ";
-              $result = mysqli_query($connexion, $q);
-              while($row = mysqli_fetch_assoc($result)){
-            ?>
+      <div class="all_lieux">
+        <?php
+        $q = " SELECT * FROM lieu_touristique ";
+        $result = mysqli_query($connexion, $q);
+        while ($row = mysqli_fetch_assoc($result)) {
+        ?>
           <div class="lieu_item">
             <div class="lieu_info">
               <h1><?php echo $row['nom']; ?></h1>
-              <h2><?php echo $row['adresse'].', '.$row['contact']; ?></h2>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+              <h2><?php echo $row['adresse'] . ', ' . $row['contact']; ?></h2>
+              <p><?php echo $row['descriptions']; ?></p>
             </div>
             <div class="lieu_img">
               <img src="./admin/db_files/tourisme/<?php echo $row['photo']; ?>" alt="img">
             </div>
           </div>
-          <?php   }?>
+        <?php   } ?>
         <!-- <div class="lieu_item">
           <div class="lieu_info">
             <h1>Project 2</h1>
@@ -77,30 +77,29 @@
           </div>
         </div> -->
       </div>
-          
-          <?php 
-            /* echo ("onjour"); */
-             /*  $q = " SELECT * FROM projet ";
+
+      <?php
+      /* echo ("onjour"); */
+      /*  $q = " SELECT * FROM projet ";
               $result = mysqli_query($connexion, $q);
               while($row = mysqli_fetch_assoc($result)){ */
-            ?>
-          <!--   <div  class="item_cont">
-              <h2 class="sub_title"><?php /*  echo $row['titre']  */?></h2>
+      ?>
+      <!--   <div  class="item_cont">
+              <h2 class="sub_title"><?php /*  echo $row['titre']  */ ?></h2>
               <p class="hist_text"><?php /* echo $row['descriptions'] */ ?></p>
             </div> -->
-            <?php  /* } */?>
-        </div>
-      </div>
-      
-    </section>
-    <!-- end presentation section -->
+      <?php  /* } */ ?>
+    </div>
+    </div>
 
-    
-    
-  </main>
-  
-  <!-- footer sec -->
-  <?php require_once("./includes/footer.php"); ?>
+  </section>
+  <!-- end presentation section -->
 
-  <!-- end footer sec -->
- 
+
+
+</main>
+
+<!-- footer sec -->
+<?php require_once("./includes/footer.php"); ?>
+
+<!-- end footer sec -->
